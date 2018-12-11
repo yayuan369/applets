15548dc92e06b6275c1445252bce8abfb517be95
jinglijuan <lijuan.jing@meditrusthealth.com>
2018-12-11 16:10:44 +0800

    暂存

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App'
 
+import store from '@/store'
+var Fly=require("flyio/dist/npm/wx")
+var fly=new Fly
+Vue.prototype.$http = fly
+
 Vue.config.productionTip = false
+
+import IboxPlugin from '@/plugins/ibox'
+Vue.use(IboxPlugin)
 App.mpType = 'app'
 
-const app = new Vue(App)
+const app = new Vue({
+    store,
+    ...App
+})
 app.$mount()
+
+export default{
+    config :{
+        page:[],
+    }
+}
